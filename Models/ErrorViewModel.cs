ccc3e4e2e306053ed38b9b18d94c928469dce95a
ddarkeh <ddarkeh1@gmail.com>
2019-03-31 22:12:54 +0100

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gwasanaeth.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
